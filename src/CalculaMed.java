439ee827852d2a0ea1bb8cc84abb52511a64747a
Felipeaards <felipeaards@gmail.com>
2026-09-04 23:05:28 -0300

    Proxima atualizacao

--- src/CalculaMed.java
+++ src/CalculaMed.java
@@ -13,15 +13,18 @@ public class CalculaMed {
         float n1, n2, n3, media;
 
         // Atribuição
+         System.out.println("Seu nome: ");
         String nome;
         nome = input.next(); //next() simula a troca de linha de uma máquina de escrever
          // Esse cara serve para parar de pegar o texto quando encontrar um espaço (Ex: Em Felipe Augusto ele só pega Felipe)
         // n1 = (float)7.8; // Dizendo de forma explicita que essa variável é do tipo float de fomar clara
         // n2 = 8.9f; // o "f" no final é a mesma coisa que fizemos no comando acima
         // n3 = 4.8f;
-
+         System.out.println("Nota 01: ");
          n1 = input.nextFloat();
+         System.out.println("Nota 02: ");
          n2 = input.nextFloat();
+         System.out.println("Nota 03: ");
          n3 = input.nextFloat();
 
         // Processamento
